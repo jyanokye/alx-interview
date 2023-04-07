425230b6f0e9bcc4bec78563319a91726484a6d2
Anokye James Yaw <jamesyawanokye17@gmail.com>
2023-04-07 03:33:38 +0000

    lovely

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,14 +1,24 @@
 #!/usr/bin/python3
-"""minimum operations"""
+"""Minimum operations"""
 
-def minOperations(n):
-"""function for the minimum operations"""
-    dp = [0] * (n + 1)
-    for i in range(2, n + 1):
-        dp[i] = float('inf')
-        for j in range(2, int(i ** 0.5) + 1):
-            if i % j == 0:
-                dp[i] = min(dp[i], dp[j] + (i // j))
-                dp[i] = min(dp[i], dp[i // j] + j)
-        dp[i] = min(dp[i], dp[i - 1] + 1)
-    return dp[n]
+def min_operations(n):
+    """
+       function for the minimum
+       operation
+       return:
+              minimum number of operations
+    """ 
+    if n < 2:
+        return 0
+
+    operations, factor = 0, 2
+
+    while factor <= n:
+        if n % factor == 0:
+            operations += factor
+            n //= factor
+            factor -= 1
+
+        factor += 1
+
+    return operations
