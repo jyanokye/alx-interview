84aaa41f4db118a62810cb115a001c659c1bde33
jyanokye <101642075+jyanokye@users.noreply.github.com>
2023-04-24 19:34:26 +0000

    Create 0-nqueens.py

---
+++ 0x05-nqueens/0-nqueens.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""chessboard"""
+import sys
+
+def queens(n, i=0, a=[], b=[], c=[]):
+    if i < n:
+        for j in range(n):
+            if j not in a and i + j not in b and i - j not in c:
+                yield from queens(n, i + 1, a + [j], b + [i + j], c + [i - j])
+    else:
+        yield a
+
+def solve(n):
+    for solution in queens(n, 0):
+        print([[i, s] for i, s in enumerate(solution)])
+
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        print("Usage: nqueens N")
+        sys.exit(1)
+
+    try:
+        n = int(sys.argv[1])
+    except ValueError:
+        print("N must be a number")
+        sys.exit(1)
+
+    if n < 4:
+        print("N must be at least 4")
+        sys.exit(1)
+
+    solve(n)
