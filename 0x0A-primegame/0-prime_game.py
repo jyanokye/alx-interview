b578664b0b64dc7eea39a9b57e514f1288612d0e
jyanokye <101642075+jyanokye@users.noreply.github.com>
2023-05-29 18:23:48 +0000

    Update 0-prime_game.py

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,6 +1,19 @@
 #!/usr/bin/python3
+"""Winner is prime"""
+
 
 def isWinner(x, nums):
+    """
+    Determines the winner between two players, Maria and Ben, based on a list of numbers.
+
+    Args:
+        x: Unused argument.
+        nums (list): A list of numbers.
+
+    Returns:
+        str or None: The name of the winner ("Maria" or "Ben"), or None in case of a tie.
+
+    """
     wins = {"Maria": 0, "Ben": 0}
     for n in nums:
         is_maria_turn = True
